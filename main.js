492f38ba58e8f19aa9da1159a2ba1d15643df7ca
Konstantin Pak <konstantin13@gmail.com>
2023-12-03 01:00:54 +0600

    added sidebar

--- main.js
+++ main.js
@@ -1,88 +1,120 @@
- 
- //handle setupevents as quickly as possible
- const setupEvents = require('./installers/setupEvents')
- if (setupEvents.handleSquirrelEvent()) {
+//handle setup events as quickly as possible
+const setupEvents = require('./installers/setupEvents')
+if (setupEvents.handleSquirrelEvent()) {
     // squirrel event handled and app will exit in 1000ms, so don't do anything else
     return;
- }
+}
 
-const electron = require('electron')
 // Module to control application life.
-var path = require('path')
-const app = electron.app
+let path = require('path')
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
-const {Tray, Menu, nativeImage } = require('electron')
+const {BrowserView, BrowserWindow, appTray, Menu, nativeImage, app, Tray} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let sidebar
+let mainView
 
-var i18n = new(require('./translations/i18n'))
+let i18n = new (require('./translations/i18n'))
 
 app.disableHardwareAcceleration()
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    titleBarStyle: 'hidden-inset',
-    width: 1280,
-    height: 800,
-    minWidth: 1280,
-    minHeight: 800,
-    //backgroundColor: '#312450',
-    //show: false,
-    icon: path.join(__dirname, 'assets/icons/48.ico'),
-    // webPreferences: {
-    //   offscreen: true
-    // }
-})
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        titleBarStyle: 'hidden-inset',
+        width: 1520,
+        height: 850,
+        minWidth: 1520,
+        minHeight: 850,
+        useContentSize: true,
+        //backgroundColor: '#312450',
+        //show: false,
+        icon: path.join(__dirname, 'assets/icons/48.ico'),
+        // webPreferences: {
+        //   offscreen: true
+        // }
+    })
 
-  // and load the index.html of the app.
-  //mainWindow.loadURL(`https://uchet.kz/`)
-  mainWindow.loadFile('index.html')
-  setTimeout(() => mainWindow.loadURL(`https://uchet.kz/`), 1000)
-  //if (navigator.onLine) {mainWindow.loadURL(`https://uchet.kz/`)} else {mainWindow.loadURL(`index.html`)} 
+    // and load the index.html of the app.
+    //mainWindow.loadURL(`https://uchet.kz/`)
+    //mainWindow.loadFile('index.html')
+    //setTimeout(() => mainWindow.loadURL(`https://uchet.kz`), 1000)
+    //if (navigator.onLine) {mainWindow.loadURL(`https://uchet.kz`)} else {mainWindow.loadURL(`index.html`)}
 
-  // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
+    const sidebarWidth = 240
+    sidebar = new BrowserView()
+    mainWindow.addBrowserView(sidebar)
+    sidebar.setBounds({x: 0, y: 0, width: sidebarWidth, height: mainWindow.getBounds().height})
+    sidebar.setAutoResize({width: true, height: false});
+    sidebar.webContents.loadFile('sidebar.html')
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainView = new BrowserView()
+    mainWindow.addBrowserView(mainView)
+    sidebar.setAutoResize({width: true, height: false});
+    mainView.setBounds({
+        x: 300,
+        y: 0,
+        width: mainWindow.getBounds().width - sidebarWidth,
+        height: mainWindow.getBounds().height
+    })
+    mainView.webContents.loadURL('https://uchet.kz/month/')
 
-  require('./mainmenu')
+    // catch resize event emitted on window
+    mainWindow.on('resize', function () {
 
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show()
-})
+        // store window's new size in variable
+        let newBounds = mainWindow.getBounds()
+
+        // set BrowserView's bounds explicitly
+        sidebar.setBounds({x: 0, y: 0, width: sidebarWidth, height: newBounds.height})
+        mainView.setBounds({x: sidebarWidth, y: 0, width: newBounds.width - sidebarWidth, height: newBounds.height})
+    })
+
+    // Open the DevTools.
+    //mainWindow.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+
+    require('./mainmenu')
+
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
+
+
 }
 
 function get() {
-  return mainWindow;
+    return mainWindow;
 }
 
 // Export the publicly available functions.
 module.exports = {get};
 
 app.whenReady().then(() => {
-  //const icon = nativeImage.createFromPath()
-  const tray = new Tray('./assets/icons/24.ico')
+    //const icon = nativeImage.createFromPath()
+    const tray = new Tray(path.join(__dirname, "assets", "icons", "24.ico"))
 
-  const trayMenu = Menu.buildFromTemplate([
-    {
-      label: i18n.__('Close'),
-      click: () => { app.quit() }
-    }
-  ])
+    const trayMenu = Menu.buildFromTemplate([
+        {
+            label: i18n.__('Close'),
+            click: () => {
+                app.quit()
+            }
+        }
+    ])
 
-  tray.setContextMenu(trayMenu)
-  tray.setToolTip('Uchet Desktop')
-  tray.setTitle('Uchet Desktop')
+    tray.setContextMenu(trayMenu)
+    tray.setToolTip('Uchet Desktop')
+    tray.setTitle('Uchet Desktop')
 })
 
 // This method will be called when Electron has finished
@@ -92,19 +124,21 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        //mainWindow.removeBrowserView(mainView)
+        //mainWindow.removeBrowserView(sidebar)
+        app.quit()
+    }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
--- mainmenu.js
+++ mainmenu.js
@@ -1,138 +1,142 @@
 const {Menu, nativeImage} = require('electron')
 const electron = require('electron')
 const app = electron.app
-var i18n = new(require('./translations/i18n.js'))
-var path = require('path')
+let i18n = new (require('./translations/i18n.js'))
+let path = require('path')
 
 const mainWindow = require(path.join(__dirname, 'main'));
 
 const template = [
-  {
-    label: i18n.__('View'),
-    submenu: [
-      {
-        role: 'resetzoom', label: i18n.__('Actual size')
-      },
-      {
-        role: 'zoomin', label: i18n.__('Zoom in')
-      },
-      {
-        role: 'zoomout', label: i18n.__('Zoom out')
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'togglefullscreen', label: i18n.__('Toggle fullscreen')
-      }
-    ]
-  },
-  {
-    role: 'window', label: i18n.__('Window'),
-    submenu: [
-      {
-        role: 'minimize', label: i18n.__('Minimize')
-      },
-      {
-        role: 'close', label: i18n.__('Close')
-      }
-    ]
-  },
-  {type:'separator'}, 
-  {
-    label: i18n.__('Go Back'),
-    click: () => { mainWindow.get().webContents.goBack(); }
-  },
-  {
-    label: i18n.__('Go Forward'),
-    //icon: nativeImage.createFromPath('./assets/icons/24.ico'),
-    click: () => { mainWindow.get().webContents.goForward(); }
-  },
-  // {
-  //   role: 'help', label: i18n.__('Help'),
-  //   submenu: [
-  //     {
-  //       label: i18n.__('Learn more'),
-  //       click () { require('electron').shell.openExternal('https://github.com/crilleengvall/electron-tutorial-app') }
-  //     }
-  //   ]
-  // }
+    {
+        label: i18n.__('View'),
+        submenu: [
+            {
+                role: 'resetzoom', label: i18n.__('Actual size')
+            },
+            {
+                role: 'zoomin', label: i18n.__('Zoom in')
+            },
+            {
+                role: 'zoomout', label: i18n.__('Zoom out')
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'togglefullscreen', label: i18n.__('Toggle fullscreen')
+            }
+        ]
+    },
+    {
+        role: 'window', label: i18n.__('Window'),
+        submenu: [
+            {
+                role: 'minimize', label: i18n.__('Minimize')
+            },
+            {
+                role: 'close', label: i18n.__('Close')
+            }
+        ]
+    },
+    {type: 'separator'},
+    {
+        label: i18n.__('Go Back'),
+        click: () => {
+            mainWindow.get().webContents.goBack();
+        }
+    },
+    {
+        label: i18n.__('Go Forward'),
+        //icon: nativeImage.createFromPath('./assets/icons/24.ico'),
+        click: () => {
+            mainWindow.get().webContents.goForward();
+        }
+    },
+    // {
+    //   role: 'help', label: i18n.__('Help'),
+    //   submenu: [
+    //     {
+    //       label: i18n.__('Learn more'),
+    //       click () { require('electron').shell.openExternal('https://github.com/crilleengvall/electron-tutorial-app') }
+    //     }
+    //   ]
+    // }
 ]
 
 if (process.platform === 'darwin') {
-  const name = app.getName()
-  template.unshift({
-    label: name,
-    submenu: [
-      {
-        role: 'about', label: i18n.__('About') + " " + app.getName()
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'services', label: i18n.__('Services'),
-        submenu: []
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'hide', label: i18n.__('Hide') + " " + app.getName()
-      },
-      {
-        role: 'hideothers', label: i18n.__('Hide others')
-      },
-      {
-        role: 'unhide', label: i18n.__('Unhide')
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'quit', label: i18n.__('Quit') + " " + app.getName()
-      }
-    ]
-  })
-  template[1].submenu.push(
-    {
-      type: 'separator'
-    },
-    {
-      label: i18n.__('Speech'),
-      submenu: [
+    const name = app.getName()
+    template.unshift({
+        label: name,
+        submenu: [
+            {
+                role: 'about', label: i18n.__('About') + " " + app.getName()
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'services', label: i18n.__('Services'),
+                submenu: []
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'hide', label: i18n.__('Hide') + " " + app.getName()
+            },
+            {
+                role: 'hideothers', label: i18n.__('Hide others')
+            },
+            {
+                role: 'unhide', label: i18n.__('Unhide')
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'quit', label: i18n.__('Quit') + " " + app.getName()
+            }
+        ]
+    })
+    template[1].submenu.push(
         {
-          role: 'startspeaking', label: i18n.__('Start speaking')
+            type: 'separator'
         },
         {
-          role: 'stopspeaking', label: i18n.__('Stop speaking')
+            label: i18n.__('Speech'),
+            submenu: [
+                {
+                    role: 'startspeaking', label: i18n.__('Start speaking')
+                },
+                {
+                    role: 'stopspeaking', label: i18n.__('Stop speaking')
+                }
+            ]
         }
-      ]
-    }
-  )
-  template[3].submenu = [
-    {
-      label: i18n.__('Close'),
-      accelerator: 'CmdOrCtrl+W',
-      role: 'close'
-    },
-    {
-      label: i18n.__('Minimize'),
-      accelerator: 'CmdOrCtrl+M',
-      role: 'minimize'
-    },
-    {
-      label: i18n.__('Zoom'),
-      role: 'zoom'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      label: i18n.__('Bring all to front'),
-      role: 'front'
-    }
-  ]
+    )
+    template[3].submenu = [
+        {
+            label: i18n.__('Close'),
+            accelerator: 'CmdOrCtrl+W',
+            role: 'close'
+        },
+        {
+            label: i18n.__('Minimize'),
+            accelerator: 'CmdOrCtrl+M',
+            role: 'minimize'
+        },
+        {
+            label: i18n.__('Zoom'),
+            role: 'zoom'
+        },
+        {
+            type: 'separator'
+        },
+        {
+            label: i18n.__('Bring all to front'),
+            role: 'front'
+        }
+    ]
 }
 
 const menu = Menu.buildFromTemplate(template)
